5643085ef5af3bf24269ece379825bec962ba9b6
Geo Fratian <84881414+ElPsyKurisu@users.noreply.github.com>
2024-01-19 12:01:18 -0500

    added a depreciated folder and remade a new exp file
    Also figure out you can use regex in find and replace for spaces and tabs #bless

---
+++ PVloop.py
@@ -0,0 +1,221 @@
+import numpy as np
+import time
+import os
+from ekpy.control import core
+from ekpy.control.instruments import keysight81150a, keysightdsox3024a
+from scipy import interpolate
+import scipy.integrate as it
+import pandas as pd
+from ekpy.control import plotting
+import matplotlib.pyplot as plt
+
+"""
+This program is used to generate a dataset on a FE sample to then calculate Hysteresis. It works by sending in a series of triangle pulses
+and then meauring the current and the voltage across the capacitor sample. Connect channel 1 of the wavegen to channel 1 of the scope, and connect
+channel 2 of the wavegen in parallel with the sample which is in series with channel 2 of the scope. 
+"""
+
+
+
+def pv_hysteresis_wf(initial_delay, freq, pulse_delay, voltage, qtimepoints=12):
+    '''
+    Creates the waveform used to calculate the hysteresis of the sample, should create 2 sawtooth waveforms followed
+    by 2 inverted ones. This code was taken from labview and alot of it is not needed but since it works we shall leave
+    it as is for now. Despite the fact we never use q_time 
+    '''
+    #first build Qtime
+    initial_delay = float(initial_delay)
+    freq = float(freq)
+    pulse_delay = float(pulse_delay)
+    voltage = float(voltage)
+    q_time = []
+    t_increment = 0.0005/freq
+    sumsofar = initial_delay
+    q_time.append(sumsofar)
+    count = 0
+    for i in range(qtimepoints - 1):
+        if count == 2:
+            count = 0
+            value = sumsofar + pulse_delay
+        else:
+            value = sumsofar + t_increment
+            count += 1
+        q_time.append(value)
+        sumsofar = value
+    #Now we build the time_array
+    time_array = []
+    time_array.append(0)
+    time_array.append(initial_delay)
+    t_increment2 = 0.00025/freq
+    for i in range(4):
+        sumsofar2 = 0
+        last_element = time_array[-1]
+        sumsofar2 = t_increment2 + last_element
+        time_array.append(sumsofar2)
+        sumsofar2 += t_increment
+        time_array.append(sumsofar2)
+        sumsofar2 += t_increment2
+        time_array.append(sumsofar2)
+        sumsofar2 += pulse_delay
+        time_array.append(sumsofar2)
+    
+    #Now we calculate the Waveform frequency
+    waveform_freq = 1/time_array[-1]
+    #Now we calculate the interpolated time [This is a dumb method probably way easier way to do it]
+    interp_time = []
+    interp_time.append(0)
+    iteration_count = 10000 - 1
+    for i in range(iteration_count):
+        interp_time.append((i + 1) * (time_array[-1]/iteration_count))
+    #note this was the same shit as doing like np.linearinterpolate or linspace with max being the last element
+
+    #Now we make a voltage array
+    voltage_array = voltage*np.array([0,0,1,-1,0,0,1,-1,0,0,-1,1,0,0,-1,1,0,0])
+    temp = interpolate.interp1d(time_array, voltage_array)
+    interp_voltage_array = temp(interp_time)
+    #only thing we use from this is interp_voltage_array wtf
+    return q_time, time_array, waveform_freq, interp_time, interp_voltage_array
+
+def setup_scope(scope, time_scale, voltage_channel, current_channel,
+                voltage_scale, current_scale):
+    keysightdsox3024a.initialize(scope)
+    keysightdsox3024a.configure_timebase(scope, time_base_type='MAIN', reference='CENTer', scale=f'{time_scale}', position=f'{5*time_scale}')
+    keysightdsox3024a.configure_channel(scope, channel=voltage_channel, vertical_scale=voltage_scale, impedance='FIFT')#set both to 50ohm
+    keysightdsox3024a.configure_channel(scope, channel=current_channel, vertical_scale=voltage_scale, impedance='FIFT')
+    #NOTE changing the position now to 5* the timebase to hopefully get the full signal
+    keysightdsox3024a.configure_trigger_characteristics(scope, trigger_source='EXT', low_voltage_level='0.75', high_voltage_level='0.95', sweep='NORM')
+    keysightdsox3024a.configure_trigger_edge(scope, trigger_source='EXT', input_coupling='DC')
+
+    
+
+def setup_wavegen(wavegen, voltage_channel, interp_voltage_array, waveform_freq, voltage):
+    keysight81150a.initialize(wavegen)
+    keysight81150a.configure_impedance(wavegen, voltage_channel, source_impedance='50.0', load_impedance='50')
+    keysight81150a.configure_impedance(wavegen, '2', source_impedance='50.0', load_impedance='50') #changed load from 1000000
+    keysight81150a.configure_trigger(wavegen, voltage_channel, source='MAN')
+    keysight81150a.configure_output_amplifier(wavegen, voltage_channel)
+    keysight81150a.configure_output_amplifier(wavegen, '2')
+    keysight81150a.create_arb_wf(wavegen, interp_voltage_array, 'PV')
+    keysight81150a.configure_arb_wf(wavegen, voltage_channel, 'PV', gain=f'{voltage*2}', freq=f'{waveform_freq}') 
+    keysight81150a.configure_arb_wf(wavegen, '2', 'PV', gain=f'{voltage*2}', freq=f'{waveform_freq}')
+
+
+def run_function(scope, wavegen, initial_delay, pulse_delay, freq, voltage, 
+                 capacitor_area, thickness, permittivity, amplification, 
+                 voltage_channel:str='1', current_channel:str='2'):
+    """Run function for FEHysteresis expirement. 
+    
+    NOTE MIGHT WANT TO ADD TO BE ABLE TO ACCEPT FOR FREQ: '10KHZ' SYNTAX LIKE BEFORE.
+
+    args:
+        wavegen (pyvisa.resources.gpib.GPIBInstrument): Keysight 81150a
+        scope (pyvisa.resources.gpib.GPIBInstrument): Keysight DSOX3024A
+        initial_delay (str): Intial delay in seconds
+        pulse_delay (str): Same as Tdelay in seconds
+        freq (str): Frequency, assumed in kHz unless a suffix is used (need to add logic)
+        v_end (str): Voltage in units of volts (for vertical scale of osc), gets divided by 4 to scale voltage/div. Is later divided by loop_count to get the voltage step for each subsequent loop.
+        capacitor_area (str): In units of m^2 use scientific notation (maybe add logic for suffixes)
+        thickness (str): In units of m use scientific notation
+        permittivity (str): Dielectric constant, gets multiplied by epsilon_0
+        amplification (str): How much to amplify capacitor formula by used to calculate the current chnnl resolution which I dont use rn
+    returns:
+        (FE): Experiment
+        
+        
+        notes:
+        basicaly just calculate capacitance of sample from basic formula"""
+    #NOTE I do not use current_chnnl_resolution at all in erics code
+    capacitance = float(capacitor_area)*float(permittivity)*8.854e-12/float(thickness)
+    current_chnnl_resolution = capacitance*50*float(amplification)
+    if current_chnnl_resolution < 0.008: #changed from 0.008
+        current_chnnl_resolution = 0.008
+    time_scale = (float(initial_delay) + (float(pulse_delay) * 4) + (16 * (1/(float(freq) * 4000)))) / 10
+    voltage_channel_scale = float(voltage)/4
+    #make sure that current scale >= 0.008
+    setup_scope(scope, time_scale, voltage_channel, current_channel, f'{voltage_channel_scale}', f'{current_chnnl_resolution}')
+    #Loop over the loop count
+    voltage = float(voltage)
+    #first we need to setup the wavegen
+    q_time_arr, time_array, waveform_freq, interp_time_arr, interp_voltage_array = pv_hysteresis_wf(initial_delay, freq, pulse_delay, voltage)
+    setup_wavegen(wavegen, voltage_channel, interp_voltage_array, waveform_freq, voltage)
+    #NOW we actually take the data, send out the pulse and get the data, f labview for this part
+    #Now we need to enable the wavegen, then acquire the data
+    keysightdsox3024a.initiate(scope)
+    keysight81150a.enable_output(wavegen)
+    keysight81150a.send_software_trigger(wavegen)
+    scope.query("*OPC?")
+    keysightdsox3024a.setup_wf(scope, source='CHAN1')
+    metadata_v, time_v, wfm_v = keysightdsox3024a.query_wf(scope) #currently times out here
+    time.sleep(.2)
+    keysightdsox3024a.setup_wf(scope, source='CHAN2')
+    metadata_c, time_c, wfm_c = keysightdsox3024a.query_wf(scope)
+
+    exp_params_meta_data = {
+            'capacitance':capacitance,
+            'initial_delay': initial_delay,
+            'pulse_delay': pulse_delay,
+            'frequency': freq,
+            'waveform_freq': waveform_freq,
+            'voltage': voltage,
+            'amplification': amplification,
+        }
+    meta_data = {'exp_params': exp_params_meta_data, 'scope_c': metadata_c, 'scope_v': metadata_v}
+    #How to structure the data that comes out, its going to be in a pandas df, but how. Kind of want to make it nested with raw and pv but idk
+    df = pd.DataFrame({'time_v':time_v, 'wfm_v':wfm_v, 'time_c':time_c, 'wfm_c':wfm_c})
+    base_name = 'fe_pv_'
+    return base_name, meta_data, df
+
+class FEHysteresis(core.experiment):
+    """Experiment class for running creating hysteresis loops of PV. 
+
+    args:
+        wavegen (pyvisa.resources.gpib.GPIBInstrument): Keysight 81150a
+        scope (pyvisa.resources.gpib.GPIBInstrument): Keysight DSOX3024A
+        run_function (function): Run function.
+
+    returns:
+        (FE): Experiment
+
+    """
+
+    def __init__(self, wavegen, scope, run_function=run_function):
+        super().__init__(run_function)
+        self.run_function = run_function
+        self.wavegen = wavegen
+        self.scope = scope
+        return
+    
+    def _plot(self, data, scan_params):
+        if hasattr(self, 'fig') and hasattr(self, 'ax'):
+            pass
+        else:
+            fig, ax = plt.subplots()
+            self.fig = fig
+            self.ax = ax
+        self.ax.scatter(data['time_v'], data['wfm_v'], color='blue')
+        plt.show(self.fig)
+        plotting.update_plot(self.fig)
+        return
+
+
+    def checks(self, params):
+        """Checks during initialization."""
+        if self.wavegen != params['wavegen']:
+            try:
+                raise ValueError('wavegen provided in initialization ({}) does not match that provided as an argument for run_function ({})'.format(self.wavegen, params['wavegen']))
+
+            except KeyError:
+                raise ValueError('wavegen provided in initialization ({}) does not match that provided as an argument for run_function ({})'.format(self.wavegen, None))
+
+        
+        if self.scope != params['scope']:
+            try:
+                raise ValueError('scope provided in initialization ({}) does not match that provided as an argument for run_function ({})'.format(self.scope, params['scope']))
+
+            except KeyError:
+                raise ValueError('scope provided in initialization ({}) does not match that provided as an argument for run_function ({})'.format(self.scope, None))
+        
+    def terminate(self):
+        """Termination."""
+        keysight81150a.stop(self.wavegen)
+        return

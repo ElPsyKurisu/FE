bd0605831812a6cff63f1c584cb48cb6b352a15b
Geo Fratian <84881414+ElPsyKurisu@users.noreply.github.com>
2024-03-14 18:18:24 -0400

    fixed some data
    Added better instrucitons for physical; connections

--- PVloop.py
+++ PVloop.py
@@ -12,7 +12,8 @@ import matplotlib.pyplot as plt
 """
 This program is used to generate a dataset on a FE sample to then calculate Hysteresis. It works by sending in a series of triangle pulses
 and then meauring the current and the voltage across the capacitor sample. Connect channel 1 of the wavegen to channel 1 of the scope, and connect
-channel 2 of the wavegen in parallel with the sample which is in series with channel 2 of the scope. 
+channel 2 of the wavegen in parallel with the sample which is in series with channel 2 of the scope. AKA connect channel 2 of the wavegen to port A via
+BNC and connect channel 2 of the scope to port B also via BNC (no bannannas needed)
 """
 
 
